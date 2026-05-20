f6d6481f4f1681df419e8a3aca07da66d352b7b9
hyyga <aureliacalistta537@gmail.com>
2026-05-20 13:35:13 +0700

    green

--- src/online_exam.py
+++ src/online_exam.py
@@ -1,26 +1,11 @@
 def evaluate_exam(
     exam_duration,
-    accommodation_status,
+    accomodation_status,
     disconnect_duration,
     login_time,
     is_public_holiday
 ):
-    # if exam_duration < 0:
-    #     raise ValueError("exam_duration cannot be negative")
-
-    if disconnect_duration < 0:
-        raise ValueError("disconnect_duration cannot be negative")
-
-    # if login_time < 0:
-    #     raise ValueError("login_time cannot be negative")
-
-    # if is_public_holiday:
-    #     return "Exam Canceled"
-
-    # if login_time > 30:
-    #     return "Login Rejected"
-
-    # green 
     if disconnect_duration > 5:
         return "Auto Submit Exam"
 
+    return "Exam Accepted"
